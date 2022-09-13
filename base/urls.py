1d473f27d927369ffb94496593f84d73aef9f2ef
Dimas Abreu <dimasabreu@live.com>
2022-09-12 23:47:40 -0300

    adicionei uma primary key

--- base/urls.py
+++ base/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.home, name="home"),
-    path('room/', views.room, name="room")
+    path('room/<str:pk>/', views.room, name="room")
 ]
 
