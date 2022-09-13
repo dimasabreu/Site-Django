d9883315b6fdc475af4fd403fbef56bf2981dad8
Dimas Abreu <dimasabreu@live.com>
2022-09-12 22:00:56 -0300

    main folder modify

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -1,6 +1,12 @@
 from django.contrib import admin
 from django.urls import path
+from django.http import HttpResponse
+
+def home(request):
+    return HttpResponse('Home page')
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', home),
 ]
