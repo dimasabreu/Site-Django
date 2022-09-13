75bad2b7d2a922e661bd9c789b53e19b4a0e5017
Dimas Abreu <dimasabreu@live.com>
2022-09-12 22:39:55 -0300

    Adicionado url local da base na url principal

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -1,12 +1,10 @@
 from django.contrib import admin
-from django.urls import path
-from django.http import HttpResponse
+from django.urls import path, include
 
-def home(request):
-    return HttpResponse('Home page')
+
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', home),
+    path('', include('base.urls'))
 ]
