b5cf84cf459c727bdad2860281d3139e9029b842
Dimas Abreu <dimasabreu@live.com>
2022-09-13 14:58:41 -0300

    atualização do views

--- base/views.py
+++ base/views.py
@@ -1,22 +1,15 @@
 from django.shortcuts import render
+from .models import Room
 
-rooms = [
-    {'id':1, 'name': 'Python'},
-    {'id':2, 'name': 'Java'},
-    {'id':3, 'name': 'PHP'},
-    
-]
 
 
 def home(request):
+    rooms = Room.objects.all()
     contexto = {'rooms': rooms}
     return render(request,'base/home.html', contexto)
 
 
 def room(request, pk):
-    room = None
-    for i in rooms:
-        if i['id'] == int(pk):
-            room = i
-        contexto = {'room': room}
+    room = Room.objects.get(id=pk)
+    contexto = {'room': room}
     return render(request, 'base/room.html', contexto)
