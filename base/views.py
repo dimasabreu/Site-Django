c8cc663f1f6ae2ac2b3cdb14008b8ad8fb73faa9
Dimas Abreu <dimasabreu@live.com>
2022-09-12 23:48:19 -0300

    criei as salas e arrumei pra usar render

--- base/views.py
+++ base/views.py
@@ -1,10 +1,22 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
+rooms = [
+    {'id':1, 'name': 'Python'},
+    {'id':2, 'name': 'Java'},
+    {'id':3, 'name': 'PHP'},
+    
+]
 
 
 def home(request):
-    return HttpResponse('Home page')
+    contexto = {'rooms': rooms}
+    return render(request,'base/home.html', contexto)
 
 
-def room(request):
-    return HttpResponse('ROOM')
+def room(request, pk):
+    room = None
+    for i in rooms:
+        if i['id'] == int(pk):
+            room = i
+        contexto = {'room': room}
+    return render(request, 'base/room.html', contexto)
