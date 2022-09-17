0e8778b1126798ec9223f5e3c0ca960ef06c64b6
Dimas Abreu <dimasabreu@live.com>
2022-09-17 13:59:04 -0300

    views adicionada o request sala no home

--- base/views.py
+++ base/views.py
@@ -1,11 +1,14 @@
 from django.shortcuts import render, redirect
-from .models import Room
+from .models import Room, Topic
 from .forms import RoomForm
 
 
 def home(request):
-    rooms = Room.objects.all()
-    contexto = {'rooms': rooms}
+    q = request.GET.get('q') if request.GET.get('q') != None else ''
+    rooms = Room.objects.filter(topic__name__icontains=q)
+    
+    topics = Topic.objects.all()
+    contexto = {'rooms': rooms, 'topics': topics}
     return render(request,'base/home.html', contexto)
 
 
