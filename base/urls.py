0b4f789f81aed2fdc53fede67c07c4d7ff7dc417
Dimas Abreu <dimasabreu@live.com>
2022-09-16 22:51:16 -0300

    adicionei o formulario

--- base/urls.py
+++ base/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.home, name="home"),
-    path('room/<str:pk>/', views.room, name="room")
+    path('room/<str:pk>/', views.room, name="room"),
+    path('create-room/', views.createRoom, name="create-room"),
 ]
 
