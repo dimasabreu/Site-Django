dcaac436cfd6f8e54e723769e65c842874e92830
Dimas Abreu <dimasabreu@live.com>
2022-09-17 19:33:16 -0300

    adicionando parametros de busca

--- base/views.py
+++ base/views.py
@@ -1,11 +1,14 @@
 from django.shortcuts import render, redirect
 from .models import Room, Topic
 from .forms import RoomForm
-
+from django.db.models import Q
 
 def home(request):
     q = request.GET.get('q') if request.GET.get('q') != None else ''
-    rooms = Room.objects.filter(topic__name__icontains=q)
+    rooms = Room.objects.filter(
+        Q(topic__name__icontains=q) | 
+        Q(name__icontains=q) |
+        Q(description__icontains=q))
     
     topics = Topic.objects.all()
     contexto = {'rooms': rooms, 'topics': topics}
