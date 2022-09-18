656d9c5c9b4bd22f9236f4b002b49f37ff194fda
Dimas Abreu <dimasabreu@live.com>
2022-09-18 19:11:32 -0300

    login e logout

--- base/urls.py
+++ base/urls.py
@@ -2,6 +2,9 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
+    path('login/', views.loginPage, name="login"),
+    path('logout/', views.logoutUser, name="logout"),
+    
     path('', views.home, name="home"),
     path('room/<str:pk>/', views.room, name="room"),
     path('create-room/', views.createRoom, name="create-room"),
