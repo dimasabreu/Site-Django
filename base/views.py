8d326faa52766452b90b9135a844887a41fe8206
Dimas Abreu <dimasabreu@live.com>
2022-09-19 09:26:44 -0300

    adicionado caminho para a parte de delete de coment

--- base/views.py
+++ base/views.py
@@ -81,6 +81,7 @@ def room(request, pk):
             room=room,
             body=request.POST.get('body')
         )
+        room.participants.add(request.user)
         return redirect('room', pk=room.id)
     
     contexto = {'room': room, 'room_messages': room_messages, 'participants': participants}
@@ -128,3 +129,16 @@ def deleteRoom(request, pk):
         room.delete()
         return redirect('home')
     return render(request, 'base/delete.html', {'obj': room})
+
+
+@login_required(login_url='login')
+def deleteMessage(request, pk):
+    message = Message.objects.get(id=pk)
+    
+    if request.user != message.user:
+        return HttpResponse("Você não pode deletar esse comentario!")
+    
+    if request.method == 'POST':
+        message.delete()
+        return redirect('home')
+    return render(request, 'base/delete.html', {'obj': message})
