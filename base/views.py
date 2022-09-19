562fb6759a3d751f14804bc166d473d2893bbff7
Dimas Abreu <dimasabreu@live.com>
2022-09-18 22:32:44 -0300

    usuario e msg na tela

--- base/views.py
+++ base/views.py
@@ -73,7 +73,8 @@ def home(request):
 
 def room(request, pk):
     room = Room.objects.get(id=pk)
-    contexto = {'room': room}
+    room_messages = room.message_set.all().order_by('-created')
+    contexto = {'room': room, 'room_messages': room_messages}
     return render(request, 'base/room.html', contexto)
 
 
