65ce28591089dcdd4f312fddbe33ed32eb7d372a
Dimas Abreu <dimasabreu@live.com>
2022-09-20 00:41:34 -0300

    urlpatterns

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
-
+from django.conf import settings
+from django.conf.urls.static import static
 
 
 
@@ -9,3 +10,6 @@ urlpatterns = [
     path('', include('base.urls')),
     path('api/', include('base.api.urls'))
 ]
+
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
