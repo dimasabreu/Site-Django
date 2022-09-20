7eb9c393eceffb05ca30735ddb3a7a3d191cdcbe
Dimas Abreu <dimasabreu@live.com>
2022-09-20 19:27:35 -0300

    static

--- dashboard/settings.py
+++ dashboard/settings.py
@@ -133,8 +133,8 @@ STATICFILES_DIRS = [
 ]
 
 MEDIA_ROOT = BASE_DIR / 'static/images'
+STATIC_ROOT = BASE_DIR / 'dashboard/static'
 
-# STATIC_ROOT = 
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
